d468498bbd8efdba7a127a411d91378267bf729f
SHewitt95 <s.hewitt7@umiami.edu>
2018-02-03 14:39:23 -0500

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 /// Prints numbers depending on their divisibility with
 /// passed-in arguments.
 /// 
+/// # Goal
+/// 
+/// Print "Fizz" when a number is divisible by `first_num`.
+/// Print "Buzz" when a number is divisible by `second_num`.
+/// Print "FizzBuzz" when a number is divisible by `first_num`
+/// and `second_num`.
+/// 
+/// Otherwise, just print out the number.
+/// 
 /// # Arguments
 /// * `first_num`: i32 variable. This is one of the numbers used
 /// to determine divisibiity. Will be used for "Fizz."
