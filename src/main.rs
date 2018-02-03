00d78289559d94c9d301313695a3a89aa62cf2ca
SHewitt95 <s.hewitt7@umiami.edu>
2018-02-03 14:36:41 -0500

    Learning Rust with FizzBuzz

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+/// Prints numbers depending on their divisibility with
+/// passed-in arguments.
+/// 
+/// # Arguments
+/// * `first_num`: i32 variable. This is one of the numbers used
+/// to determine divisibiity. Will be used for "Fizz."
+/// 
+/// * `second_num`: i32 variable. This is one of the numbers
+/// used to determine divisibility. Will be used for "Buzz."
+/// 
+/// * `max`: i32 variable. This determines the highest number
+/// (exclusively) `fizzbuzz` will calculate.
+fn fizzbuzz(first_num: i32, second_num: i32, max: i32) {
+    for x in 0..max {
+        if (x % (first_num * second_num) == 0) & (x > 0) {
+            println!("FizzBuzz: {}", x);
+        } else if (x % first_num == 0) & (x > 0) {
+            println!("Fizz: {}", x);
+        } else if (x % second_num == 0) & (x > 0) {
+            println!("Buzz: {}", x);
+        } else {
+            println!("{}", x);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    fizzbuzz(2, 7, 100);
 }
